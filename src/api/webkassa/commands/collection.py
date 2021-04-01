08987567f981442952dfca3e53f785c558aa709d
rsaleev <rs.aleev@gmail.com>
2021-04-01 13:36:11 +0300

    stage0

--- src/api/webkassa/commands/collection.py
+++ src/api/webkassa/commands/collection.py
@@ -1,11 +1,19 @@
-from pydantic import BaseModel
 from src.api.webkassa.exceptions import * 
-from src.db.models.token import Token
+from src.db.models import Token, Shift, States
 from src.api.webkassa.templates import TEMPLATE_ENVIRONMENT
 from src.api.webkassa.command import WebcassaCommand
 from src.api.webkassa.client import WebcassaClient
-from src.api.webkassa.commands.authorization import WebkassaClientToken
+from src.api.webkassa.commands import WebkassaClientToken
+from src.api.webkassa.models import MoneyCollectionResponse, MoneyCollectionRequest
+from xml.etree.ElementTree import fromstring
 from src import config
+import struct
+from uuid import uuid4
+from src.api.printer.commands import PrintXML, CutPresent
+import asyncio
+from src.api.webkassa import logger
+from tortoise import timezone
+
 
 """
 
@@ -13,61 +21,48 @@ Withdraw/deposit money
 
 """
 
-
-"""
-REQUEST
-"""
-
-class MoneyCollectionRequest(BaseModel):
-    token:str
-    cashbox_unique_number:str
-    operation_type:int
-    sum:int
-    external_check_number:str
-
-"""
-RESPONSE
-"""
-
-class Ofd(BaseModel):
-    name:str
-    host:str
-    code:int
-    
-class Cashbox(BaseModel):
-    unique_number:str
-    registration_number:str
-    identity_number:str
-    address:str
-    ofd:Ofd
-
-class MoneyCollectionResponse(BaseModel):
-    offline_mode:bool
-    cashbox_offline_mode:bool
-    date_time:str
-    sum:int
-    cashbox:Cashbox
-
-
-class WebkassaClientCollection:
+class WebkassaClientCollection(WebcassaCommand, WebcassaClient):
     endpoint = 'MoneyOperation'
     alias = 'deposit'
 
     @classmethod
-    def handle(cls, payload:bytearray):
-        # request = MoneyCollectionRequest(
-        #     token=Token.get(),
-        #     cashbox_unique_number=config['webkassa']['cassa_unique_number'],
-        #     operation_type=operation_type,
-        #     sum = struct.unpack('<5B', payload[4:9]),
-        #     external_check_number=str(uuid4()))
-        # response = self.dispatch(endpoint=endpoint, request_data=request, response_model=MoneyCollectionResponse, callback_error=self.exc_callback)
-        # if response:
-        #     template = TEMPLATE_ENVIRONMENT.get_or_select_template('collection.xml')
-        #     rendered = template.render(
-        #         operation_type = operation_type,
-        #         request=request,
-        #         response=response)
-        #     rendered_doc = fromstring(rendered)
-        #     return rendered
-        pass
+    async def handle(cls, payload:bytearray, operation_type:int):
+        token = await Token.get(id=1)
+        request = MoneyCollectionRequest(
+            token=token.token,
+            cashbox_unique_number=config['webkassa']['cassa_unique_number'],
+            operation_type=operation_type,
+            sum = struct.unpack('<5B', payload[4:9]),
+            external_check_number=str(uuid4()))
+        try:
+            response = cls.dispatch(endpoint=cls.endpoint, 
+                                    request_data=request, 
+                                    response_model=MoneyCollectionResponse, #type: ignore
+                                    callback_error=cls.exc_callback)
+            if response:
+                template = TEMPLATE_ENVIRONMENT.get_or_select_template('collection.xml')
+                rendered = fromstring(template.render(
+                    operation_type = operation_type,
+                    request=request,
+                    response=response))
+                await PrintXML.handle(rendered)
+                await CutPresent.handle()
+        except Exception as e:
+            await logger.exception(e)
+            return 
+
+    @classmethod
+    async def exc_callback(cls, exc, payload):
+        if isinstance(exc, CredentialsError):
+            await States.filter(id=1).update(gateway=0)
+            return False
+        elif isinstance(exc, UnrecoverableError):
+            await States.filter(id=1).update(gateway=0)
+            return False
+        elif isinstance(exc, ExpiredTokenError):
+            response = await WebkassaClientToken.handle()
+            if response:
+                payload.token = response
+                return True
+            else:
+                return False
