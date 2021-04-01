f223c2ad1a53c2c2085b0f82e867ec831085c80a
rsaleev <rs.aleev@gmail.com>
2021-04-01 13:47:11 +0300

    dfdf

--- deployment/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# new persmission to usb printer
-FILE = "/etc/udev/rules.d/00-usb-persmissions.rules"
-touch $FILE
-echo "1 SUBSYSTEM=="usb", ATTRS{idVendor}=="0dd4", ATTRS{idProduct}=="015d", MODE="0664", GROUP="dialout" > $FILE
