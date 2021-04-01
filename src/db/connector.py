67c5d016149284ccd7bd027acac963d46a759b2f
rsaleev <rs.aleev@gmail.com>
2021-04-01 13:34:39 +0300

    stage0

--- src/db/connector.py
+++ src/db/connector.py
@@ -6,7 +6,7 @@ from tortoise import timezone
 class DBConnector:
 
     def __init__(self):
-        self.db = None
+        self.db = Tortoise()
         self.connected = Tortoise._inited
         
     async def connect(self):
@@ -17,18 +17,21 @@ class DBConnector:
         Returns:
             [type]: [description]
         """
-        await Tortoise.init(
+
+        await self.db.init(
             db_url=f'sqlite://{os.path.abspath(os.getcwd())}/{os.environ.get("SQLITE_DB")}',
             modules={'models': ['src.db.models']}
         )
         
         # Generate the schema
-        await Tortoise.generate_schemas()
+        await self.db.generate_schemas()
 
         # initialize records
         await Shift.get_or_create(defaults={'open_date':timezone.now()}, id=1)
         await States.get_or_create(id=1)
         await Token.get_or_create(defaults={'token':'', 'ts':timezone.now()})
-        self.db = Tortoise._connections
-        self.connected = Tortoise._inited
+        
         return self
+
+    async def disconnect(self):
+        await self.db.close_connections()
