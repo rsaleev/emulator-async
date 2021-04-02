4651fb2197171ec62d961a5c88e3a075832b8c6a
rsaleev <rs.aleev@gmail.com>
2021-04-02 13:12:11 +0300

    stage0

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from src.db.connector import DBConnector
 from src.api.printer.device import Printer
 from src.api.shtrih.device import Paykiosk
 from src.api.watchdog import Watchdog
+from src.api.printer.commands import PrinterFullStatusQuery
+from src.api.webkassa.commands import WebkassaClientTokenCheck
 from src import logger
 import signal
 import asyncio
@@ -45,20 +47,33 @@ class Application:
 
     @classmethod
     async def init(cls):
+        await logger.warning('Initializing application...')
+
         loop = asyncio.get_running_loop()
         try:
             task_db_connect = cls.db.connect()
             task_fiscalreg_connect = cls.fiscalreg.connect()
             task_printer_connect = loop.run_in_executor(None, cls.printer.connect)
             await asyncio.gather(task_db_connect, task_fiscalreg_connect, task_printer_connect)
+            task_printer_check = PrinterFullStatusQuery.handle()
+            task_webkassa_check = WebkassaClientTokenCheck.handle()
+            await asyncio.gather(task_printer_check, task_webkassa_check)
         except Exception as e:
             await logger.exception(e)
+        else:
+            await logger.warning('Application initialized')
+
    
     @classmethod
     async def serve(cls):
+        await logger.info('Serving...')
         while True:
-            await asyncio.ensure_future(cls.fiscalreg.serve())
-            await asyncio.ensure_future(cls.watchdog.poll())
+            try:
+                await asyncio.ensure_future(cls.fiscalreg.serve())
+                await asyncio.ensure_future(cls.watchdog.poll())
+            except Exception as e:
+                await logger.exception(e)
+
         
             
 if __name__ == '__main__':
@@ -71,3 +86,4 @@ if __name__ == '__main__':
                                                         app._signal_handler(s, loop)))
     loop.run_until_complete(app.init())
     loop.run_until_complete(app.serve())
+    
