d0fdc4e1a9d8ba53949c17cd68774b6b7009c7db
rsaleev <rs.aleev@gmail.com>
2021-04-06 20:34:06 +0300

    fixed graphics barcode

--- src/api/printer/commands/graphics.py
+++ src/api/printer/commands/graphics.py
@@ -31,7 +31,7 @@ class PrintGraphicLines(Printer):
 
     @classmethod
     def _print_graphics(cls, payload:bytearray):
-        arr = struct.unpack(f'<{len(payload)}B', payload)[6:]
+        arr = struct.unpack(f'<{len(payload)}B', payload)[6:66]
         img = Image.frombytes(data=bytes(arr), size=(len(arr)*8,1), mode='1')
         repeats = struct.unpack('<2B', payload[4:6])[0]
         bc = Image.new(mode='1', size=(len(arr)*8, repeats))
@@ -39,3 +39,7 @@ class PrintGraphicLines(Printer):
             bc.paste(img, (0,i))
         bc_inverted = ImageOps.invert(bc.convert('L'))
         Printer().image(bc_inverted, impl=cls.impl, center=cls.center)
+
+     
+
+     
