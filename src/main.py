e92310893421b645e9ac147aa1c0dbeecb2042d1
rsaleev <rs.aleev@gmail.com>
2021-04-10 16:08:23 +0300

    test1

--- src/main.py
+++ src/main.py
@@ -89,5 +89,4 @@ if __name__ == '__main__':
         loop.add_signal_handler(s, lambda: asyncio.ensure_future(app._signal_handler(s, loop)))
     loop.run_until_complete(app.init())
     loop.run_until_complete(app.serve())
-    loop.run_forever()
    
