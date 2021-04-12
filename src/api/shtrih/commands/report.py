ba37bb988a5112fed2ad8c1b538cdaa6b04cee39
rsaleev <rs.aleev@gmail.com>
2021-04-12 17:37:14 +0300

    stage4

--- src/api/shtrih/commands/report.py
+++ src/api/shtrih/commands/report.py
@@ -9,23 +9,14 @@ class ZReport(ShtrihCommand, ShtrihCommandInterface):
     _command_code = bytearray((0x41,))
             
     @classmethod
-    def handle(cls, payload:bytearray)->Tuple[Coroutine, Coroutine]:
-        task_process = cls._process(payload)
-        task_execute = cls._dispatch()
-        return task_process, task_execute
-
-    @classmethod
-    async def _process(cls, payload:bytearray) ->bytearray:
+    async def handle(cls, payload:bytearray):
+        await WebkassaClientZReport.handle()
         arr = bytearray()
         arr.extend(cls._length)
         arr.extend(cls._command_code)
         arr.extend(cls._error_code)
         arr.extend(cls._password)
         return arr
-
-    @classmethod
-    async def _dispatch(cls)->None:
-        await WebkassaClientZReport.handle()
        
 class XReport(ShtrihCommand, ShtrihCommandInterface):
     _length = bytearray((0x03,))
