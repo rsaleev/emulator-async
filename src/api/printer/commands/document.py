e7f0dc84bcd9dab4e64a68a61c5ece8a91fab206
rsaleev <rs.aleev@gmail.com>
2021-04-12 22:29:21 +0300

    changed submode to 5, iter over elements

--- src/api/printer/commands/document.py
+++ src/api/printer/commands/document.py
@@ -36,22 +36,10 @@ class PrintXML(Printer):
             buffer (bool, optional): perform printing in buffer or bypass. Defaults to True.
         """
         loop = asyncio.get_running_loop()
-        task_submode_change = States.filter(id=1).update(submode=5)
-        task_print_buffer = loop.run_in_executor(None, cls._print_doc, payload)
-        await asyncio.gather(task_submode_change, task_print_buffer)
-
-
-    @classmethod
-    def _print_doc(cls, payload:Element):
-        """_print_doc private scope method that handles XML document print by iteration over its' elements
-
-
-        Args:
-            payload (Element): XML object
-            buffer (bool): perform printing in buffer or bypass. From argument of higher level method
-        """
+        await States.filter(id=1).update(submode=5)
+        await asyncio.sleep(0.1)
         for child in payload:
-            cls._print_element(child)
+            await loop.run_in_executor(None, cls._print_element, child)
 
     @classmethod
     def _print_element(cls, content:Element):
@@ -63,40 +51,44 @@ class PrintXML(Printer):
             content (Element): XML object Element
             buffer (bool): perform printing in buffer or bypass. From argument of higher level method
         """
-        align = content.attrib.get('align', 'left')
-        bold = True if content.attrib.get('text', False)  and content.attrib['text']== 'bold' else False
-        if content.tag != 'br' and content.attrib.get('text', False): 
-            content.text = content.text.replace(u'\u201c','"')
-            content.text = content.text.replace(u'\u201d', '"')
-            content.text = content.text.replace(u'\u202f', ' ')
-            if cls.buffer:
-                if config['printer']['doc']['send_encoding']:
-                    if cls.encoding_output == 'cp1251':
-                        Printer()._raw(cls.codepage_command.extend(cls.CP1251)) 
-                    elif cls.encoding_output == 'cp866':
-                        Printer().buffer._raw(cls.codepage_command.extend(cls.CP866)) 
-                Printer().buffer.set(align=align, font=cls.font,bold=bold, width=cls.width, height=cls.height, custom_size=cls.custom_size) #type: ignore          
-                output = content.text.encode(cls.encoding_output)
-                Printer().buffer._raw(output) 
-            else:
-                if config['printer']['doc']['send_encoding']:
-                    if cls.encoding_output == 'cp1251':
-                        Printer()._raw(cls.codepage_command.extend(cls.CP1251))
-                    elif cls.encoding_output == 'cp866':
-                        Printer()._raw(cls.codepage_command.extend(cls.CP866)) 
-                    Printer()._raw(cls.codepage_command) 
-                Printer().set(align=align, font=cls.font,bold=bold, width=cls.width, height=cls.height, custom_size=cls.custom_size) #type: ignore 
-                output = content.text.encode(cls.encoding_output)         
-                Printer()._raw(output) 
-        elif content.tag == 'br':
-            if cls.buffer:
-                Printer().buffer._raw(bytes("\n", 'ascii'))             
-            else:
-                Printer()._raw(bytes("\n", 'ascii'))  
-        elif content.tag == 'qr':
-            if cls.buffer:
-                Printer().buffer.qr(unquote(str(content.text)))
-            else:
-                Printer().qr(unquote(str(content.text)))
+        try:
+            align = content.attrib.get('align', 'left')
+            bold = True if content.attrib.get('text', False)  and content.attrib['text']== 'bold' else False
+            if content.tag != 'br' and content.attrib.get('text', False): 
+                content.text = content.text.replace(u'\u201c','"')
+                content.text = content.text.replace(u'\u201d', '"')
+                content.text = content.text.replace(u'\u202f', ' ')
+                if cls.buffer:
+                    if config['printer']['doc']['send_encoding']:
+                        if cls.encoding_output == 'cp1251':
+                            Printer()._raw(cls.codepage_command.extend(cls.CP1251)) 
+                        elif cls.encoding_output == 'cp866':
+                            Printer().buffer._raw(cls.codepage_command.extend(cls.CP866)) 
+                    Printer().buffer.set(align=align, font=cls.font,bold=bold, width=cls.width, height=cls.height, custom_size=cls.custom_size) #type: ignore          
+                    output = content.text.encode(cls.encoding_output)
+                    Printer().buffer._raw(output) 
+                else:
+                    if config['printer']['doc']['send_encoding']:
+                        if cls.encoding_output == 'cp1251':
+                            Printer()._raw(cls.codepage_command.extend(cls.CP1251))
+                        elif cls.encoding_output == 'cp866':
+                            Printer()._raw(cls.codepage_command.extend(cls.CP866)) 
+                        Printer()._raw(cls.codepage_command) 
+                    Printer().set(align=align, font=cls.font,bold=bold, width=cls.width, height=cls.height, custom_size=cls.custom_size) #type: ignore 
+                    output = content.text.encode(cls.encoding_output)         
+                    Printer()._raw(output) 
+            elif content.tag == 'br':
+                if cls.buffer:
+                    Printer().buffer._raw(bytes("\n", 'ascii'))             
+                else:
+                    Printer()._raw(bytes("\n", 'ascii'))  
+            elif content.tag == 'qr':
+                if cls.buffer:
+                    Printer().buffer.qr(unquote(str(content.text)))
+                else:
+                    Printer().qr(unquote(str(content.text)))
+        except Exception as e:
+            logger.exception(e)
+            raise e
 
                     
