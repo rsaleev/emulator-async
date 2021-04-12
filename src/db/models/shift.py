cd74c372ebdf77860b4d2a43ac9b47ed742394bb
rsaleev <rs.aleev@gmail.com>
2021-04-12 15:11:16 +0300

    stage4

--- src/db/models/shift.py
+++ src/db/models/shift.py
@@ -6,7 +6,7 @@ from tortoise.fields.data import IntField, DatetimeField
 
 class Shift(Model):
     id = IntField(pk=True)
-    open_date = DatetimeField(default=datetime.now())
+    open_date = DatetimeField(default=datetime.now)
     total_docs = IntField(default=0)
 
     class Meta:
