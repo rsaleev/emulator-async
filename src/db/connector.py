c15e25976bb4d9c8d9c3eff3270fc259f4e022df
rsaleev <rs.aleev@gmail.com>
2021-04-12 22:34:08 +0300

    use timezone, localzone from OS

--- src/db/connector.py
+++ src/db/connector.py
@@ -20,33 +20,13 @@ class DBConnector:
 
         await self.db.init(
             db_url=f'sqlite://{os.path.abspath(os.getcwd())}/{os.environ.get("SQLITE_DB")}',
-            use_tz=True, 
+            use_tz=False, 
             timezone = str(tzlocal.get_localzone()),
             modules={'models': ['src.db.models']}
         )
         
         # Generate the schema
         await self.db.generate_schemas()
-        # initialize records
-        try:
-            shift = Shift(id=1)
-            await shift.save()
-        except Exception as e:
-            await logger.error(e)
-            pass
-        try:
-            states = States(id=1)
-            await states.save()
-        except Exception as e:
-            await logger.error(e)
-            pass
-        try:
-            token = Token(id=1)
-            await token.create(id=1)
-        except Exception as e:
-            await logger.error(e)
-            pass
-        return self
 
     async def disconnect(self):
         await self.db.close_connections()
