d83951aece929fcbe1ff07d577286727510c5d05
rsaleev <rs.aleev@gmail.com>
2021-04-12 15:17:39 +0300

    fixed timezone issue

--- src/db/connector.py
+++ src/db/connector.py
@@ -2,6 +2,7 @@ from tortoise import Tortoise
 import os 
 from src.db.models import *  
 from src import logger 
+import tzlocal
 class DBConnector:
 
     def __init__(self):
@@ -20,6 +21,7 @@ class DBConnector:
         await self.db.init(
             db_url=f'sqlite://{os.path.abspath(os.getcwd())}/{os.environ.get("SQLITE_DB")}',
             use_tz=False, 
+            timezone = str(tzlocal.get_localzone()),
             modules={'models': ['src.db.models']}
         )
         
