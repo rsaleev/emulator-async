8f0414134bb2f9dcba705092fea8003e982d5602
rsaleev <rs.aleev@gmail.com>
2021-04-14 17:14:14 +0300

    added deferred printing when header is off

--- src/api/shtrih/commands/print.py
+++ src/api/shtrih/commands/print.py
@@ -2,7 +2,7 @@ import re
 import asyncio
 from uuid import uuid4
 from src import config
-from src.api.printer.commands import PrintBytes, CutPresent, PrintBuffer, ClearBuffer, PrintGraphicLines
+from src.api.printer.commands import PrintBytes, CutPresent, PrintBuffer, PrintDeferredBytes, PrintGraphicLines
 from src.api.shtrih.command import ShtrihCommand, ShtrihCommandInterface
 from src.db.models.receipt import Receipt
 
@@ -28,16 +28,28 @@ class PrintDefaultLine(ShtrihCommand, ShtrihCommandInterface):
         arr.extend(cls._password)
         return arr
         
+  
+
+    class NoChangeHeader:
+        line1=r"^Невыданная сдача\W+"
+        line2=r"^\W+Банкноты\W+\d+\W+\d+\w+"
+        line3=r"^\W+Монеты\W+\d+\W+\d+\w+"
+        line4=r"^\W+Недоступно\W+\d+\W+\d+\w+"
+        lines = [line1, line2, line3, line4]
+
     @classmethod
     async def _parse_custom_line(cls, payload:bytearray) -> None:
-        line = re.match(pattern=config['webkassa']['receipt']['regex'],  
-                        string=bytes(payload[5:]).decode('cp1251'), 
+        line_to_print = bytes(payload[5:]).decode('cp1251')
+        ticket = re.match(pattern=config['webkassa']['receipt']['regex'],  
+                        string=line_to_print, 
                         flags=re.IGNORECASE)
-        if line:
-            num = line.group(2)
+        if ticket:
+            num = ticket.group(2)
             await Receipt.create(uid=uuid4(), ticket=num)
-            if not config['webkassa']['receipt']['header']:
-                asyncio.ensure_future(ClearBuffer.handle())
+        for line in cls.NoChangeHeader.lines:
+            data = re.match(line,line_to_print)
+            if data:
+               await PrintDefferdBytes.handle(bytes(line_to_print, 'cp1251')) #type: ignore Pylance
 
 class PrintOneDimensionalBarcode(ShtrihCommand, ShtrihCommandInterface):
     _length =  bytearray((0x03,))
@@ -67,13 +79,13 @@ class Cut(ShtrihCommand, ShtrihCommandInterface):
     @classmethod
     async def handle(cls, payload:bytearray) -> bytearray:
         try:
-            if config['printer']['text']['buffer']:
-                await PrintBuffer.handle()
-                await ClearBuffer.handle()
-            await CutPresent.handle()
+            # wait for execution:
+            # if error occured -> return 0x200
+            asyncio.wait_for(PrintBuffer.handle(), timeout=3)
         except:
             cls.set_error(200) # printer error: no connection or no signal from sensors
         else:
+            await CutPresent.handle()
             cls.set_error(0)
         arr = bytearray()
         arr.extend(cls._length)
