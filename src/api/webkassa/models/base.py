b1586add83b2f12603f13a2f064d90053c2d92a5
rsaleev <rs.aleev@gmail.com>
2021-04-14 17:15:52 +0300

    added comments

--- src/api/webkassa/models/base.py
+++ src/api/webkassa/models/base.py
@@ -11,6 +11,16 @@ class WebcassaOutputErrors(BaseModel):
         allow_population_by_field_name = True
 
 class WebcassaOutput(BaseModel):
+    """WebcassaOutput 
+
+    Basic response model {'Data':...,} or {'Errors':[...]}
+
+    Response will be converted to object and 
+    then concrete model will be used to parse Data into the concrete object concerned to operation
+
+    Args:
+        BaseModel ([type]): [description]
+    """
     data: Optional[Any] = None
     errors: Optional[List[WebcassaOutputErrors]] = None
 
