37ca7ad7dd4f1f6401620b7d2317b5173ba5b3c6
rsaleev <rs.aleev@gmail.com>
2021-04-16 17:18:36 +0300

    fixed logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -34,45 +34,6 @@ class CustomAsyncFormatter(Formatter):
         return result
 
 
-class CustomSyncFormatter(logging.Formatter):
-    def __init__(self):
-        super().__init__(fmt="%(levelno)d: %(msg)s", datefmt=None, style='%')
-
-    def format(self, record):
-        # Save the original format configured by the user
-        # when the logger formatter was instantiated
-        format_orig = self._style._fmt
-        # Replace the original format with one customized by logging level
-        if record.levelno == logging.DEBUG:
-            self._style._fmt = "DEBUG: %(asctime)s LINE:%(lineno)d FUNC:%(funcName)s MSG:%(msg)s"
-
-        elif record.levelno == logging.INFO:
-            self._style._fmt = "INFO: %(asctime)s %(msg)s"
-
-        elif record.levelno == logging.ERROR:
-            self._style._fmt = "ERROR: %(asctime)s %(msg)s"
-
-        elif record.levelno == logging.WARNING:
-            self._style._fmt = "WARNING: %(asctime)s %(msg)s"
-        # Call the original formatter class to do the grunt work
-        result = logging.Formatter.format(self, record)
-        # Restore the original format configured by the user
-        self._style._fmt = format_orig
-        return result
-class SynchronousLogger(logging.Logger):
-    def __init__(self, filename:str, name: str = __name__):
-        super().__init__(name=name,
-                        level=getattr(logging, os.environ.get('LOG_LEVEL', 'INFO')))
-        custom_formatter = CustomSyncFormatter()
-        fileHandler = TimedRotatingFileHandler(filename,
-                                                when="midnight",
-                                                interval=1,
-                                                backupCount=14,
-                                                encoding='utf8')
-        fileHandler.setFormatter(custom_formatter)
-        self.addHandler(fileHandler)
-        self.setLevel(self.level)
-
 class AsynchronousLogger(aiologger.Logger):
     def __init__(self,  filename:str,name:str = __name__):
         super().__init__(name=name, level=getattr(logging, os.environ.get('LOG_LEVEL', 'INFO')))
