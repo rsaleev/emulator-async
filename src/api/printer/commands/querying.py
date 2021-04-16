3115788ad20eecfb8d87429a54f576d3a308cc3f
rsaleev <rs.aleev@gmail.com>
2021-04-16 18:18:06 +0300

    fixed

--- src/api/printer/commands/querying.py
+++ src/api/printer/commands/querying.py
@@ -31,7 +31,7 @@ class PrinterFullStatusQuery(Printer):
         asyncio.ensure_future(logger.debug(
             f'PAPER:{int(paper)}|ROLL:{int(roll)}|COVER:{int(cover)}|RECOVERABLE:{int(rec_err)}|UNRECOVERABLE:{int(unrec_err)}'))
         # if paper not presented or cover is opened or unrecoverable/recoverable error exist -> submode=1
-        if not paper and not cover and not rec_err and not unrec_err:
+        if paper and not cover and not rec_err and not unrec_err:
             asyncio.ensure_future(States.filter(id=1).update(submode=0, paper=int(paper), cover=int(cover), roll=int(roll), jam=int(rec_err)))
             return False
         # otherwise do not update submode value, assert that some operation in progress
