266b09ec48a0006b454a605b06c6e1035192dbbf
rsaleev <rs.aleev@gmail.com>
2021-04-20 15:29:22 +0300

    QR size from config

--- src/api/printer/commands/graphics.py
+++ src/api/printer/commands/graphics.py
@@ -1,8 +1,8 @@
 import struct
-import asyncio
 from PIL import Image, ImageOps
 from src import config
 from src.api.printer.device import Printer
+from src.api.printer import logger
 
 class PrintQR(Printer):
 
@@ -11,7 +11,7 @@ class PrintQR(Printer):
 
     @classmethod
     async def handle(cls, payload:str):
-        Printer().buffer.qr(content=payload, center=True, size=5)
+        Printer().buffer.qr(content=payload, center=True, size=cls.size)
 
 
 class PrintGraphicLines(Printer):
@@ -31,20 +31,26 @@ class PrintGraphicLines(Printer):
         Args:
             payload (bytearray): incoming raw data from Fiscal device
         """
-        arr = struct.unpack(f'<{len(payload)}B', payload)[6:66]
-        # generates 1 pixel heigth image
-        img = Image.frombytes(data=bytes(arr), size=(len(arr)*8,1), mode='1')
-        # pixel data is repeated several times
-        repeats = struct.unpack('<2B', payload[4:6])[0]
-        # prepare new image
-        bc = Image.new(mode='1', size=(len(arr)*8, repeats))
-        # append 1 px data several times (repeats)
-        for i in range(0,repeats):
-            bc.paste(img, (0,i))
-        # invert colors black/white
-        bc_inverted = ImageOps.invert(bc.convert('L'))
-        Printer().buffer.image(bc_inverted, impl=cls.impl, center=cls.center)
-        Printer().hw('INIT')
+        logger.debug('Rendering graphics')
+        try:
+            arr = struct.unpack(f'<{len(payload)}B', payload)[6:66]
+            # generates 1 pixel heigth image
+            img = Image.frombytes(data=bytes(arr), size=(len(arr)*8,1), mode='1')
+            # pixel data is repeated several times
+            repeats = struct.unpack('<2B', payload[4:6])[0]
+            # prepare new image
+            bc = Image.new(mode='1', size=(len(arr)*8, repeats))
+            # append 1 px data several times (repeats)
+            for i in range(0,repeats):
+                bc.paste(img, (0,i))
+            # invert colors black/white
+            bc_inverted = ImageOps.invert(bc.convert('L'))
+        except Exception as e:
+            logger.exception(e)
+        else:
+            Printer().buffer.image(bc_inverted, impl=cls.impl, center=cls.center)
+            logger.debug('Buffering graphics')
+
 
 
      
