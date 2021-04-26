953c9ea945bfe3cbd874e6da87bffed932661d0e
rsaleev <rs.aleev@gmail.com>
2021-04-26 18:10:47 +0300

    sale/close v2

--- src/api/shtrih/commands/sale.py
+++ src/api/shtrih/commands/sale.py
@@ -117,10 +117,42 @@ class SimpleCloseSale(ShtrihCommand, ShtrihCommandInterface):
         arr.extend(cls._password)
         arr.extend(change)
         return arr
+
+
+
+class OpenSale2(ShtrihCommand, ShtrihCommandInterface):
+    _length = bytearray((0x01,))
+    _command_code = bytearray((0xFF,0x46))
+
+    @classmethod
+    async def handle(cls, payload:bytearray) ->bytearray:
+        try:
+            count = struct.unpack('<i2B',payload[5:11])[0]//10**3
+            price = struct.unpack('<iB', payload[11:16])[0]//10**2
+            tax_percent = config['webkassa']['taxgroup'][str(payload[27])]
+            tax = round(price*count/(100+int(tax_percent))*tax_percent,2)
+            receipt = await Receipt.filter(ack=False).annotate(max_value = Max('id')).first()
+            if receipt.id: #type: ignore
+                await Receipt.filter(id=receipt.id).update(count=count, price=price, tax_percent=tax_percent, tax=tax) #type:ignore
+            # create record with empty ticket number
+            else:
+                await Receipt.create(uid=uuid4(), ticket='', count=count, price=price, tax_percent=tax_percent, tax=tax)
+            if not config['webkassa']['receipt']['header']:
+                asyncio.ensure_future(ClearBuffer.handle())
+        except Exception as e:
+            asyncio.ensure_future(logger.exception(e))
+            cls.set_error(3)
+        else:
+            cls.set_error(0)
+        arr = bytearray()
+        arr.extend(cls._length)
+        arr.extend(cls._command_code)
+        arr.extend(cls._error_code)
+        return arr 
         
-class AdvancedCloseReceipt(ShtrihCommand, ShtrihCommandInterface):
+class CloseReceipt2(ShtrihCommand, ShtrihCommandInterface):
     _length = bytearray((0x08,))# B[1] LEN - 1 byte
-    _command_code = bytearray((0xFF,0x46)) #B[2] - 1 byte
+    _command_code = bytearray((0x8E, )) #B[2] - 1 byte
 
     @classmethod
     async def handle(cls, payload:bytearray):
