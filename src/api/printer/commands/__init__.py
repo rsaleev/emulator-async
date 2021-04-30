cda034b85ae126f69f815f0003205743632fa8b3
rsaleev <rs.aleev@gmail.com>
2021-04-30 15:35:15 +0300

    fixed imports

--- src/api/printer/commands/__init__.py
+++ src/api/printer/commands/__init__.py
@@ -2,8 +2,3 @@ from src.api.printer.commands.printing import PrintBytes, PrintXML, PrintDeferre
 from src.api.printer.commands.querying import ClearBuffer, PrintBuffer, PrinterFullStatusQuery, ModeSetter
 from src.api.printer.commands.present import CutPresent
 from src.api.printer.commands.graphics import PrintGraphicLines, PrintQR
-
-
-COMMANDS = [PrintXML, PrintBytes, PrintBuffer, 
-            PrinterFullStatusQuery, PrintBuffer, ClearBuffer, CutPresent,
-            PrintGraphicLines, PrintQR]
