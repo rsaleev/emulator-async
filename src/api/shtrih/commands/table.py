7234f8a9eca5e0499edf3de92e7601cf58c23eb5
rsaleev <rs.aleev@gmail.com>
2021-05-18 12:49:43 +0300

    fixed serial number message

--- src/api/shtrih/commands/table.py
+++ src/api/shtrih/commands/table.py
@@ -1,32 +1,27 @@
-
 import struct
-import asyncio
 from src.api.shtrih.command import ShtrihCommand, ShtrihCommandInterface
 
 class SerialNumber(ShtrihCommand, ShtrihCommandInterface):
    
     _length = bytearray((0x17,))# B[1] LEN - 1 byte
     _command_code = bytearray((0xFF,0x02,)) #B[2] - 2 byte
-    _fn_number = struct.pack('<16B',*[0x30]*16)
+    _fn_number = struct.pack('<16B',*bytearray((0x30,))*16)
     
     @classmethod
-    async def handle(cls, payload):
+    async def handle(cls):
         arr = bytearray()
         arr.extend(cls._length)
         arr.extend(cls._command_code)
         arr.extend(cls._error_code)
         arr.extend(cls._fn_number)
         return arr
-
-
-
 class TableModify(ShtrihCommand, ShtrihCommandInterface):
 
     _length = bytearray((0x02,))
     _command_code = bytearray((0x1E,))
 
     @classmethod
-    async def handle(cls, payload):
+    async def handle(cls):
         arr = bytearray()
         arr.extend(cls._length)
         arr.extend(cls._command_code)
