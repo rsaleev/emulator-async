5bd510dde7f72c45b704342cd12310a1f419bf38
rsaleev <rs.aleev@gmail.com>
2021-05-20 16:09:53 +0300

    added missing commands

--- src/api/shtrih/commands/__init__.py
+++ src/api/shtrih/commands/__init__.py
@@ -1,7 +1,7 @@
-from src.api.shtrih.commands.print import PrintDefaultLine, Cut
+from src.api.shtrih.commands.print import PrintDefaultLine, Cut, PrintOneDimensionalBarcode
 from src.api.shtrih.commands.report import ZReport, XReport
 from src.api.shtrih.commands.shift import OpenShift, CloseShift
-from src.api.shtrih.commands.sale import OpenReceipt, OpenSale, SimpleCloseSale, OpenSale2, CloseReceipt2
+from src.api.shtrih.commands.sale import CancelReceipt, OpenReceipt, OpenSale, SimpleCloseSale, OpenSale2, CloseReceipt2
 from src.api.shtrih.commands.subtotal import SubTotal
 from src.api.shtrih.commands.state import FullState
 from src.api.shtrih.commands.table import SerialNumber, TableModify
@@ -9,6 +9,8 @@ from src.api.shtrih.commands.collection import Deposit, Withdraw
 
 
 
-COMMANDS =[PrintDefaultLine, ZReport, XReport, OpenReceipt, OpenSale, OpenShift, CloseShift, SimpleCloseSale,
-            OpenSale2, CloseReceipt2,
-            SubTotal, FullState, SerialNumber, Deposit, Withdraw, Cut, TableModify]
+COMMANDS =[PrintDefaultLine, Cut, PrintOneDimensionalBarcode,
+            ZReport, XReport, 
+            OpenReceipt, OpenSale, SimpleCloseSale,OpenSale2, CloseReceipt2,SubTotal, CancelReceipt,
+            OpenShift, CloseShift, 
+            FullState, SerialNumber, Deposit, Withdraw, TableModify]
